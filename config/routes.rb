cf0dec9cb7bc7ec682c50eb58c9af53ba7fd5410
Tyrant Davis <davistyrant@gmail.com>
2019-06-10 10:55:47 -0500

    Add Post controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/new'
+  get 'posts/edit'
   get 'welcome/index'
   get 'welcome/about'
   get 'welcome/contact'
