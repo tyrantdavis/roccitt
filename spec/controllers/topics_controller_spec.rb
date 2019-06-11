d6c4443cd1d7f55fdac83be0e88ad7b82291b00a
Tyrant Davis <davistyrant@gmail.com>
2019-06-11 16:46:29 -0500

    Implement Topics index specs and views.

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe TopicsController, type: :controller do
+  let(:new_topic) { Topic.create!( 
+    name: RandomData.random_sentence, 
+    description: RandomData.random_paragraph,
+    public: true
+    )  }
 
+  # let(:new_post)  { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it "assigns new_topic to @topics" do
+      get :index
+      expect(assigns(:topics)).to eq([new_topic])
+    end
+  end
 end
