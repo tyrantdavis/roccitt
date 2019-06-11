8b2f13f6f2003ea19e6d1100d54967177f8ac0ad
Tyrant Davis <davistyrant@gmail.com>
2019-06-11 16:06:39 -0500

    Add associations and add_topic_to_posts migration.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
-# performs a "cascade delete", which ensures that when a post is deleted, all of its comments are too.
+    belongs_to :topic
+    # performs a "cascade delete", which ensures that when a post is deleted, all of its comments are too.
     has_many :comments, dependent: :destroy
 end
