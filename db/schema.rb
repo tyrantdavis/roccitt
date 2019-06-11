0183d266f8597c8be923a93c6522853bf5a938c3
Tyrant Davis <davistyrant@gmail.com>
2019-06-11 16:03:21 -0500

    Add topic_id FK to posts table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_11_194543) do
+ActiveRecord::Schema.define(version: 2019_06_11_202341) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2019_06_11_194543) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "topic_id"
+    t.index ["topic_id"], name: "index_posts_on_topic_id"
   end
 
   create_table "questions", force: :cascade do |t|
