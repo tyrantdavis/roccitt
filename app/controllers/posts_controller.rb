ccceb8e7af9b8a2c1bd552e218e71d726a3d5887
Tyrant Davis <davistyrant@gmail.com>
2019-06-11 13:03:44 -0500

    Add test for Edit and modify Edit action.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,5 +26,6 @@ class PostsController < ApplicationController
   
 
   def edit
+    @post = Post.find(params[:id])
   end
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -35,16 +35,16 @@ RSpec.describe PostsController, type: :controller do
 
     describe "POST create" do
       it "increases the number of Post by 1" do
-        expect{post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
+        expect{post :create, params: { post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}}.to change(Post,:count).by(1)
       end
 
     it "assigns the new post to @post" do
-        post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+        post :create, params: { post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}
         expect(assigns(:post)).to eq Post.last  
     end
     
     it "redirects to the new post" do
-        post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+        post :create, params: { post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}
         expect(response).to redirect_to Post.last  
     end
   end
@@ -52,26 +52,43 @@ RSpec.describe PostsController, type: :controller do
 
   describe "GET #show" do
     it "returns http success" do
-      get :show, {id: new_post.id}
+      get :show, params: {id: new_post.id}
       expect(response).to have_http_status(:success)
     end
 
     it "renders the #show view" do
-      get :show, {id: new_post.id}
-      expect(resposne).to render_template :show  
+      get :show, params: {id: new_post.id}
+      expect(response).to render_template :show  
     end
 
     it "assigns new_post to @post" do
-      get :show, {id: new_post.id}
+      get :show, params: {id: new_post.id}
       expect(assigns(:post)).to eq(new_post)  
     end
   end
 
-  # describe "GET #edit" do
-  #   it "returns http success" do
-  #     get :edit
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET #edit" do
+    it "returns http success" do
+      get :edit, params: {id: new_post.id}
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #edit view" do
+      get :edit, params: {id: new_post.id}
+      expect(response).to render_template :edit
+    end
+
+     it "assigns post to be updated to @post" do
+       get :edit,  params: {id: new_post.id}
+ 
+       post_instance = assigns(:post)
+ 
+       expect(post_instance.id).to eq new_post.id
+       expect(post_instance.title).to eq new_post.title
+       expect(post_instance.body).to eq new_post.body
+     end
+ 
+    
+  end
 
 end
