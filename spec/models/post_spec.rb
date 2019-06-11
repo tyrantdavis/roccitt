cd43767820e524e1c7f051d145ee3f01616a46f3
Tyrant Davis <davistyrant@gmail.com>
2019-06-11 16:04:59 -0500

    Add topic specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,15 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:post) { Post.create!( title: "New Post Title", body: "New Post Body"  )  }
+  let(:name) { RandomData.random_sentence  }
+  let(:description) { RandomData.random_paragraph  }
+  let(:title) { RandomData.random_sentence  }
+  let(:body) {  RandomData.random_paragraph  }
+
+  let(:topic) { Topic.create!( name: name, description: description  )  }
+  let(:post) { topic.posts.create!( title: title, body: body  )  }
+
+  it { is_expected.to belong_to( :topic  )  }
 
   describe "attributes" do
     it "has a title attribute" do
-      expect(post).to have_attributes( title: "New Post Title"  )
+      expect(post).to have_attributes( title: title )
     end
     
     it "has a body attribute" do
-      expect(post).to have_attributes( body: "New Post Body"  )
+      expect(post).to have_attributes( body: body  )
     end
   end
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -10,6 +10,8 @@ RSpec.describe Topic, type: :model do
     public: public 
     )}
     
+    it { is_expected.to have_many(:posts) }
+
     describe "attributes" do
       it "has name, description, and public attributes" do
         expect(topic).to have_attributes( 
