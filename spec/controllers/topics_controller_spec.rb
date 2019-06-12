9169eef5ce7dd29c0f2a6afdfa5463bb7004d8bb
Tyrant Davis <davistyrant@gmail.com>
2019-06-11 19:48:15 -0500

    Show Topic.

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe TopicsController, type: :controller do
     public: true
     )  }
 
-  # let(:new_post)  { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+  # let(:new_topic)  { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
   describe "GET #index" do
     it "returns http success" do
@@ -20,4 +20,22 @@ RSpec.describe TopicsController, type: :controller do
       expect(assigns(:topics)).to eq([new_topic])
     end
   end
+
+  describe "GET #show" do
+    it "returns http success" do
+      get :show, params: {id: new_topic.id}
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #show view" do
+      get :show, params: {id: new_topic.id}
+      expect(response).to render_template :show  
+    end
+
+    it "assigns new_topic to @topic" do
+      get :show, params: {id: new_topic.id}
+      expect(assigns(:topic)).to eq(new_topic)  
+    end
+  end
+  
 end
