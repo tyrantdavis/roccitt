8383d6b7f4e12c43805aa03f9049c37bbfe4593f
Tyrant Davis <davistyrant@gmail.com>
2019-06-12 17:04:54 -0500

    Fix NoMethodError: undefined method is_at_least forPost spec.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -15,8 +15,8 @@ RSpec.describe Post, type: :model do
   it { should validate_presence_of(:topic) }
   
   # is_at_least not behaving properly
-  it { should validate_presence_of(:title).is_at_least(5).on(:create) }
-  it { should validate_presence_of(:body).is_at_least(20).on(:create) }
+  it { should validate_length_of(:title).is_at_least(5).on(:create) }
+  it { should validate_length_of(:body).is_at_least(20).on(:create) }
 
 
   describe "attributes" do
