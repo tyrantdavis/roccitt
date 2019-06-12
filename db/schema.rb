c720f1a1ed84de7b0b41dde25fd5d75efe283cb4
Tyrant Davis <davistyrant@gmail.com>
2019-06-12 17:14:44 -0500

    Generate User model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_11_202341) do
+ActiveRecord::Schema.define(version: 2019_06_12_220740) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -57,6 +57,14 @@ ActiveRecord::Schema.define(version: 2019_06_11_202341) do
     t.string "name"
     t.text "description"
     t.boolean "public", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
