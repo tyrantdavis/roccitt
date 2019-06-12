041625d50b19244b320811d34f4c430e7baf4ac6
Tyrant Davis <davistyrant@gmail.com>
2019-06-12 12:56:09 -0500

    Validate Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
     belongs_to :topic
     # performs a "cascade delete", which ensures that when a post is deleted, all of its comments are too.
     has_many :comments, dependent: :destroy
+
+   validates :title, length: { minimum: 5 }, presence: true
+   validates :body, length: { minimum: 20 }, presence: true
+   validates :topic, presence: true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,7 +9,14 @@ RSpec.describe Post, type: :model do
   let(:topic) { Topic.create!( name: name, description: description  )  }
   let(:post) { topic.posts.create!( title: title, body: body  )  }
 
-  it { is_expected.to belong_to( :topic  )  }
+  it { should belong_to( :topic  )  }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:topic) }
+  
+  it { should validate_presence_of(:title).is_at_least(5).on(:create) }
+  it { should validate_presence_of(:body).is_at_least(20).on(:create) }
+
 
   describe "attributes" do
     it "has a title attribute" do
