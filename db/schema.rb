564e9a690b5aef2d89f6e95104541bee9120f870
Tyrant Davis <davistyrant@gmail.com>
2019-06-25 12:18:05 -0500

    Add Role column to Users table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_24_221115) do
+ActiveRecord::Schema.define(version: 2019_06_25_171607) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2019_06_24_221115) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
