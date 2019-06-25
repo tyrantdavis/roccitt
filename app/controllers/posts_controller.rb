3db4791e40c23662d2a271b0fbe10d69d5a38619
Tyrant Davis <davistyrant@gmail.com>
2019-06-24 19:04:36 -0500

    Modify posts_contorller.rb and topics_controller.rb to use mass-assignment and params.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,13 +10,10 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
     @topic = Topic.find(params[:topic_id])
-
-    @post.topic = @topic
+    @post = @topic.posts.build(post_params)
     @post.user = current_user
+
     if @post.save
       flash[:notice] = "Post successfully created"
       redirect_to [@topic, @post]
@@ -33,8 +30,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
+    @post.assign_attributes(post_params)
 
     if @post.save
       flash[:notice] = "Post successfully updated"
@@ -55,5 +51,9 @@ class PostsController < ApplicationController
       render :show
     end
   end
-  
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
