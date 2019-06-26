41f028ab693b61539a261d6c228c14907cf31577
Tyrant Davis <davistyrant@gmail.com>
2019-06-25 19:38:03 -0500

    Implement #create requirements.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,28 @@
 class CommentsController < ApplicationController
+ # #10
+   before_action :require_sign_in
+ 
+   def create
+ # #11
+     @post = Post.find(params[:post_id])
+     comment = @post.comments.new(comment_params)
+     comment.user = current_user
+ 
+     if comment.save
+       flash[:notice] = "Comment saved successfully."
+ # #12
+       redirect_to [@post.topic, @post]
+     else
+       flash[:alert] = "Comment failed to save."
+ # #13
+       redirect_to [@post.topic, @post]
+     end
+   end
+ 
+   private
+ 
+ # #14
+   def comment_params
+     params.require(:comment).permit(:body)
+   end
 end
