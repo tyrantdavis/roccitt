ea461e8a6144ade2bce72862d5c5301de2a6ec2a
Tyrant Davis <davistyrant@gmail.com>
2019-06-25 20:14:56 -0500

    Add Vote model specs and validations. All passing.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,5 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :post
+   validates :value, inclusion: { in: [-1, 1], message: "%{value} is not a valid vote." }, presence: true
 end
