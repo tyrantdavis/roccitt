8fae63e50ee20de497c2245746544622b41b0c41
Tyrant Davis <davistyrant@gmail.com>
2019-10-22 10:55:50 -0500

    Seed votes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,12 +22,17 @@ require 'random_data'
 
 # Create Posts
 50.times do 
-  Post.create!(
+  post = Post.create!(
     user:   users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
     )
+ 
+ # #12
+   post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+ # #13
+   rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
 end
 
 posts = Post.all
@@ -61,3 +66,4 @@ puts "#{User.count} users created"
 puts "#{Topic.count} topics creaed. "
 puts "#{Post.count} posts created."
 puts "#{Comment.count} comments created."
+ puts "#{Vote.count} votes created"
